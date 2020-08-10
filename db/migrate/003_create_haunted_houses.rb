0c1f9cb8380126b7a5c73a5d6e924dc31aa74721
Ethan <eleetyson@gmail.com>
2020-08-10 10:36:20 -0400

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,8 +8,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :theme
       t.decimal :price
       t.boolean :family_friendly
-      t.date :opening_date
-      t.date :closing_date
+      t.datetime :opening_date
+      t.datetime :closing_date
       t.text :description
     end
   end
